c501ceeac63d9f8b5ab49e8f1d7e7d1a1af6a811
ionut123i <pisu.ionut@ymail.com>
2024-01-13 16:46:36 +0200

    first commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,148 @@
+import React,{Suspense, useRef, useState} from 'react'
+import emailjs from "@emailjs/browser"
+import { Canvas } from '@react-three/fiber'
+import Fox from '../models/FOX'
+import { Loader } from '@react-three/drei'
+import UseAlert from '../hooks/UseAlert'
+import Alert from '../components/Alert'
+
+
+const Contact = () => {
+const formRef  =useRef(null)
+const [form, setForm] = useState({name:"",email:"" ,message:""})
+const [isLoading, setisLoading] = useState(false)
+const [currentAnimation, setCurrentAnimation] = useState('idle')
+
+
+const {alert,showAlert,hideAlert}=UseAlert()
+
+const handleChange=(e)=>{
+setForm({...form,[e.target.name]:e.target.value})
+}
+
+const handleSubmit=(e)=>{
+e.preventDefault()
+
+setisLoading(true)
+setCurrentAnimation("hit")
+
+emailjs.send(
+ import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+ import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+{
+  from_name:form.name,
+  to_name:"HR",
+  form_email:form.email,
+  to_email:"pisu.ionut@ymail.com",
+  message:form.message
+},
+import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+).then(()=>{
+  setisLoading(false)
+  showAlert({show:true,text:"Message send succesfully" ,type:"succes"})
+
+
+  setTimeout(()=>{
+    hideAlert()
+  setCurrentAnimation("idle")
+  setForm({name:"",email:"",message:""})
+  },[3000])
+  
+}).catch((err)=>{
+  setisLoading(false)
+  setCurrentAnimation("idle")
+  console.log(err)
+  showAlert({show:true,text:"Something went wrong" ,type:"danger"})
+})
+}
+
+const handleFocus=()=>setCurrentAnimation("walk")
+const handleBlur=()=>setCurrentAnimation("idle")
+
+  return (
+    <section className='relarive flex lg:flex-row flex-col max-container'>
+    {alert.show&& <Alert {...alert}/>}
+
+       <div className='flex-1 min-w-[50%] flex flwx-col'>
+       <h1 className='head-text '> Get in touch</h1>
+       <form className='w-full flex flex-col  gap-7 mt-14'
+       onSubmit={handleSubmit}
+       >
+           <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='John@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows='4'
+              className='textarea'
+              placeholder='Write your thoughts here...'
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+          type='submit'
+          className='btn'
+          disabled={isLoading}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          >
+        {isLoading?"Sending...":"Send Message"}
+          </button>
+       </form>
+       </div>
+       <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+        }}
+        >
+        <directionalLight  intensity={2.5} position={[0,0,1]}/>
+        <ambientLight intensity={0.5}/>
+         <Suspense fallback={<Loader/>}>
+          <Fox
+          currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.629, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+          />
+         </Suspense>
+        </Canvas>
+       </div>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <section className="max-container ">
+      <h1 className="head-text">
+      My
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Projects
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Frontend developer and fullstack based in Romania, specializing in
+          creating and building frontend apilications and databases.
+          fdgdfgdfgdfffffffffffffffffffffffffffffffffffffffffffffffffffffgfdgdsgdfsgdg
+        </p>
+      </div>
+      </section>
+  )
+}
+
+export default Projects
